c4a1edf682c8eef4b901e41457a059f8db548243
nehal vyas <114698016+jentibhai@users.noreply.github.com>
2022-11-15 20:37:20 +0000

    My homework Week 11 heroku Nehal

---
+++ src/BrowserTesting3/ChromeBrowserTest.java
@@ -0,0 +1,44 @@
+package BrowserTesting3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeBrowserTest {
+    public static void main(String[] args) {
+        String baseUrl = "http://the-internet.herokuapp.com/login";
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //Launch the URL
+        driver.get(baseUrl);
+
+        //Maximise Window
+        driver.manage().window().maximize();
+
+        //we give implicit time to drive
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Get the title of the page
+        String title=driver.getTitle();
+        System.out.println("Page title is : " + title);
+
+        // Get current URL https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+        System.out.println("Current URL:" + driver.getCurrentUrl());
+
+        //Find the email field
+        WebElement emailField =driver.findElement(By.id("username"));
+        emailField.sendKeys("test123@gmail.com");
+
+        //Find the pasword field
+        WebElement PasswordField =driver.findElement(By.name("password"));
+        PasswordField.sendKeys("test123");
+
+        //close the browser
+        driver.quit();
+    }
+
+}
